11d24cd1e7d28ce6e9e3dcd5c2c432571d77ad63
fan_qiao <wfanqiao@qq.com>
2019-01-26 20:12:49 +0800

    添加thymeleaf

--- src/main/java/com/fanqiao/secondkill/controller/DemoController.java
+++ src/main/java/com/fanqiao/secondkill/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.fanqiao.secondkill.controller;
 
 import com.fanqiao.secondkill.result.Result;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,5 +13,11 @@ public class DemoController {
     @ResponseBody
     public Result<String> hello() {
         return Result.success("success");
+    }
+
+    @RequestMapping("/page")
+    public String page(Model model) {
+        model.addAttribute("name", "fq");
+        return "page";
     }
 }
