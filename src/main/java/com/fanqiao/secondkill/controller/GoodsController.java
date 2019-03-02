55ce2c9b94ad0d68eeef36d9496ac826620dfcf7
fan_qiao <wfanqiao@qq.com>
2019-03-02 18:17:22 +0800

    添加秒杀详情页面

--- src/main/java/com/fanqiao/secondkill/controller/GoodsController.java
+++ src/main/java/com/fanqiao/secondkill/controller/GoodsController.java
@@ -33,11 +33,41 @@ public class GoodsController {
 
 	@RequestMapping("/to_list")
     public String list(Model model, SecondkillUser secondkillUser) {
+		log.info("list 入参 secondkillUser {}", secondkillUser.toString());
     	model.addAttribute("user", secondkillUser);
 		List<GoodsVo> goodsList = goodsService.listGoodsVo();
 		model.addAttribute("goodsList", goodsList);
         return "goods_list";
     }
+
+	@RequestMapping("/to_detail/{goodsId}")
+	public String detail(Model model, SecondkillUser secondkillUser, @PathVariable("goodsId")Long goodsId) {
+
+		model.addAttribute("user", secondkillUser);
+
+		GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+		model.addAttribute("goods", goods);
+
+		long startAt = goods.getStartDate().getTime();
+		long endAt = goods.getEndDate().getTime();
+		long now = System.currentTimeMillis();
+
+		int miaoshaStatus = 0;
+		int remainSeconds = 0;
+		if(now < startAt ) {//秒杀还没开始，倒计时
+			miaoshaStatus = 0;
+			remainSeconds = (int)((startAt - now )/1000);
+		}else  if(now > endAt){//秒杀已经结束
+			miaoshaStatus = 2;
+			remainSeconds = -1;
+		}else {//秒杀进行中
+			miaoshaStatus = 1;
+			remainSeconds = 0;
+		}
+		model.addAttribute("miaoshaStatus", miaoshaStatus);
+		model.addAttribute("remainSeconds", remainSeconds);
+		return "goods_detail";
+	}
 
 
 
